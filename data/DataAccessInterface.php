3f42dd259b83ff37cd1d4be7addd3b48fdcedfa1
Marko Kruljac <marko.kruljac@degordian.com>
2015-09-11 15:02:23 +0200

    added missing use

--- data/DataAccessInterface.php
+++ data/DataAccessInterface.php
@@ -1,53 +1,95 @@
 <?php
 
-namespace app\extensions\restmodel\data;
+namespace app\extensions\restmodel\models;
 
-use app\extensions\restmodel\models\BaseRestModel;
-use yii\db\QueryInterface;
+use app\extensions\restmodel\data\DataAccessInterface;
+use yii\base\Exception;
+use yii\base\Model;
+use yii\helpers\Json;
 
-interface DataAccessInterface {
+abstract class BaseRestModel extends Model implements DataAccessInterface {
+
+    //trebaju nam eventi i nešto što će vodit računa o dirty parametrima (možda)
+    //svakako treba nešto što će znat da li je model new record ili je učitani record
+
+    public function save($runValidation = true, $attributeNames = null) {
+        if ($this->isNewRecord()) {
+            $this->insert($runValidation, $attributeNames);
+        }
+        else {
+            $this->update($runValidation, $attributeNames);
+        }
+    }
 
     /**
-     * @return QueryInterface
+     * Not yet implemented
+     * @return bool
      */
-    public function find();
+    private function isNewRecord() {
+        return true;
+    }
+
+    public function insert($runValidation = true, $attributeNames = null) {
+        $endpoint = $this->insertEndpoint();
+        $url = $this->createUrl($this->baseUrl(), $endpoint->path);
+        $requestBody = $this->serialize();
+        $this->request($endpoint->method, $url, $requestBody);
+    }
+
+    protected abstract function insertEndpoint();
+
+    private function createUrl($baseUrl, $path) {
+        $url = sprintf('%s/%s', rtrim($baseUrl, '/'), ltrim($path, '/'));
+        return $url;
+    }
 
     /**
-     * @return BaseRestModel
-     */
-    public function findOne();
-
-    /**
-     * @return BaseRestModel
-     */
-    public function findAll();
-
-    /**
-     * @param bool|true $runValidation
-     * @param null      $attributeNames
+     * The base url for your api
      *
-     * @return bool
+     * @return string
      */
-    public function save($runValidation = true, $attributeNames = null);
+    public abstract function baseUrl();
 
     /**
-     * @param bool|true $runValidation
-     * @param null      $attributeNames
-     *
-     * @return bool
+     * @todo implement strategy, jsonSerializer, xmlSerializer, itd
      */
-    public function insert($runValidation = true, $attributeNames = null);
+    private function serialize() {
+        $attributes = $this->getAttributes();
+        $json = Json::encode($attributes);
+        return $json;
+    }
 
-    /**
-     * @param bool|true $runValidation
-     * @param null      $attributeNames
-     *
-     * @return bool
-     */
-    public function update($runValidation = true, $attributeNames = null);
+    private function request($method, $url, $body = null) {
+        dump(sprintf('Request Method: %s', $method));
+        dump(sprintf('Request Url: %s', $url));
+        dump(sprintf('Request Body: %s', $body));
+    }
 
-    /**
-     * @return bool
-     */
-    public function delete();
+    public function update($runValidation = true, $attributeNames = null) {
+        $endpoint = $this->updateEndpoint();
+        $url = $this->createUrl($this->baseUrl(), $endpoint->path);
+        $requestBody = $this->serialize();
+        $this->request($endpoint->method, $url, $requestBody);
+    }
+
+    public function delete() {
+        if ($this->isNewRecord()) {
+            //vidi kako yii hendla ovo? isto i za update/insert - dal baciš exception ili svejedno probaš okinut brisanje itd ili samo ako je definiran $id odnosno deleteEndpoint
+        }
+        $endpoint = $this->deleteEndpoint();
+        $url = $this->createUrl($this->baseUrl(), $endpoint->path);
+        $this->request($endpoint->method, $url);
+    }
+
+    protected abstract function deleteEndpoint();
+
+    public function findOne($condition = null) {
+        return $this->find($condition)->one();
+    }
+
+    public function findAll($condition = null) {
+        return $this->find($condition)->all();
+    }
+
+    protected abstract function updateEndpoint();
 }
